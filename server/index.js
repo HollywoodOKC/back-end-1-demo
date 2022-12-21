93dfcfcf3effa7bbf767cac0bf092bb9d0829a01
HollywoodOKC <T_Lane11@yahoo.com>
2022-12-20 22:05:27 -0600

    Working.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 const app = express();
 
 const port = 5050;
@@ -11,7 +12,16 @@ app.use(cors());
 let inventory = ['computer', 'jacket', 'hat', 'TV', 'Mac', 'water', 'keys', 'food', 'backpack', 'wallet'];
 
 app.get("/api/inventory", (req, res) => {
+  if (inventory.includes(req.query.item)) {
+    res.status(200).send([req.query.item]);
+  }
+
   res.status(200).send(inventory);
+});
+
+app.get("/api/inventory/:index", (req, res) => {
+  const { index } = req.params;
+  res.status(200).send(inventory[index]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}, solid connection`));
